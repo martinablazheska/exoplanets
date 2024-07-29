9f0f9ba2f3235232f6c53c942612f45ed222910a
Martina Blazheska <123024212+martinablazheska@users.noreply.github.com>
2024-07-29 14:57:59 +0200

    feature: Add property names - Modified the Exoplanet, ExoplanetDto, ExoplanetMapper and ExoplanetsService to include three more planetary properties: host age, planet density and system distance

--- exoplanets-backend/src/main/java/com/martina/exoplanets/dto/ExoplanetDto.java
+++ exoplanets-backend/src/main/java/com/martina/exoplanets/dto/ExoplanetDto.java
@@ -8,9 +8,13 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 public class ExoplanetDto {
-    private String exoplanetName;
-    private String hostStarName;
-    private Double exoplanetOrbitalPeriod;
-    private Double exoplanetEarthRadius;
-    private Double exoplanetEarthMass;
+    private String exoplanetName; // name of exoplanet
+    private String hostStarName; // name of the host star
+    private Double hostStarAge; // age of the host star
+    private Double exoplanetOrbitalPeriod; // duration of a complete orbit around the host star or system (days)
+    private Double exoplanetEarthRadius; // exoplanet radius measured in units of Earth's radius
+    private Double exoplanetEarthMass; // exoplanet mass  measured in units of Earth's mass
+    private Double exoplanetDensity; // exoplanet density measured in g/cm^3
+    private Double distance; // distance to the planetary system measured in units of parasecs
+
 }
--- exoplanets-backend/src/main/java/com/martina/exoplanets/model/Exoplanet.java
+++ exoplanets-backend/src/main/java/com/martina/exoplanets/model/Exoplanet.java
@@ -8,7 +8,10 @@ import lombok.Setter;
 public class Exoplanet {
     private String pl_name;
     private String hostname;
+    private Double st_age;
     private Double pl_orbper;
     private Double pl_rade;
     private Double pl_bmasse;
+    private Double pl_dens;
+    private Double sy_dist;
 }
